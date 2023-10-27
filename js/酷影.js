1564ee9a74712636f7437827a8217a9927d09b08
shro <457898443@qq.com>
2023-10-27 02:16:55 +0000

    Update 酷影.js

--- js/酷影.js
+++ js/酷影.js
@@ -18,7 +18,7 @@
         class_name: '电影&电视剧&动漫&综艺&儿童', // 分类筛选 /api.php/app/nav
         class_url: '1&2&4&3&5',
         play_parse: true,
-        lazy:'js:if(/m3u8|mp4/.test(input)){input}else{let purl=request("http://jx.realdou.cn/home/api?type=ys&uid=1308131&key=aefgjklstzABDFQU78&url="+input);input={jx:0,url:JSON.parse(purl).url,parse:0}}',
+        lazy:'js:if(/m3u8|mp4/.test(input)){input}else{let purl=request("http://jx.realdou.cn/home/api?type=ys&uid=1308131&key=aefgjklstzABDFQU78&url="+input);input={jx:1,url:JSON.parse(purl).url,parse:1}}',
         limit: 6,
         推荐: 'json:list[0].vlist;*;*;*;*',
         一级: 'json:list;vod_name;vod_pic;vod_remarks||vod_score;vod_id',
