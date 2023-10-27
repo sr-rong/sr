8ef2703c7d68f8915faa057bac5b8e0194ba4530
shro <457898443@qq.com>
2023-10-27 02:39:25 +0000

    替换 酷影.js

--- js/酷影.js
+++ js/酷影.js
@@ -18,8 +18,10 @@
         class_name: '电影&电视剧&动漫&综艺&儿童', // 分类筛选 /api.php/app/nav
         class_url: '1&2&4&3&5',
         play_parse: true,
-        lazy:'js:if(/m3u8|mp4/.test(input)){input}else{let purl=request("http://jx.realdou.cn/home/api?type=ys&uid=1308131&key=aefgjklstzABDFQU78&url="+input);input={jx:1,url:JSON.parse(purl).url,parse:1}}',
+        lazy:'js:if(/m3u8|mp4/.test(input)){input}else{let purl=request("http://jx.realdou.cn/home/api?type=ys&uid=1308131&key=aefgjklstzABDFQU78&url="+input);input={jx:0,url:JSON.parse(purl).url,parse:0}}',
         limit: 6,
         推荐: 'json:list[0].vlist;*;*;*;*',
         一级: 'json:list;vod_name;vod_pic;vod_remarks||vod_score;vod_id',
-        二级:'*',搜索:'js:let d=[];let html=request(input);let list=JSON.parse(html).sun;for(let j=1;j<=list;j++){let t=JSON.parse(request(MY_URL+j));d.push({title:t.name,desc:t.author,img:t.img,url:t.video})}setResult(d)',}
+        二级: 'js:try{let html=request(input);print(html);html=JSON.parse(html);let node=html.data;VOD={vod_id:node["vod_id"],vod_name:node["vod_name"],vod_pic:node["vod_pic"],type_name:node["vod_class"],vod_year:node["vod_year"],vod_area:node["vod_area"],vod_remarks:node["vod_remarks"],vod_actor:node["vod_actor"],vod_director:node["vod_director"],vod_content:node["vod_content"].strip()};let episodes=node.vod_url_with_player;let playMap={};if(typeof play_url==="undefined"){var play_url=""}episodes.forEach(function(ep){let source=ep["name"];if(!playMap.hasOwnProperty(source)){playMap[source]=[]}playMap[source].append(ep["url"])});let playFrom=[];let playList=[];Object.keys(playMap).forEach(function(key){playFrom.append(key);playList.append(playMap[key])});let vod_play_from = playFrom.join("$$$");let vod_play_url=playList.join("$$$");VOD["vod_play_from"]=vod_play_from;VOD["vod_play_url"]=vod_play_url}catch(e){log("获取二级详情页发生错误:"+e.message)}',
+        搜索: '*',
+    }
