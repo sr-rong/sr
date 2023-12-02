0c846b86718720ea839035d34d1e6b43bf959aaf
shro <457898443@qq.com>
2023-12-02 06:14:12 +0000

    Update 来米.js

--- js/来米.js
+++ js/来米.js
@@ -3,7 +3,7 @@ muban.mxpro.二级.tabs = '#y-playList&&.module-tab-item';
 var rule = {
     title:'酷客影院',
     模板:'mxpro',
-    host:'https://www.laimi.me',
+    host:'http://tv.open.laimi.tv',
     // url:'/vodshow/fyclass--------fypage---.html',
     url:'/vodshow/fyclassfyfilter.html',
     filterable:1,//是否启用分类筛选,
